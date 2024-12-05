faa2fce75203dea9b695f94bf0f1a077372e2ecb
la228855 <la228855@student.helha.be>
2024-12-05 21:23:13 +0100

    Addition of the Personal Profile component + display of a user's personal profile page with its various information and buttons for validating information and deleting an account (Design)

--- ToutouApp-fronted/src/app/app.routes.ts
+++ ToutouApp-fronted/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './component/home-page/home-page.component';
 import { MapComponent } from './component/map/map.component';
+import { PersonalProfileComponent } from './component/personal-profile/personal-profile.component';
 
 export const routes: Routes = [
     {path:'',component:HomePageComponent},
-    { path:'map', component: MapComponent }
+    { path:'map', component: MapComponent },
+    { path:'personal-profile', component: PersonalProfileComponent }
 ];
