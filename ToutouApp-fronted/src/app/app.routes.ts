6749284147a7d0affac0b7ab338295418e201725
la228855 <la228855@student.helha.be>
2024-12-05 20:08:59 +0100

    Added Map component with Leaflet integration and display of static details (Design)

--- ToutouApp-fronted/src/app/app.routes.ts
+++ ToutouApp-fronted/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './component/home-page/home-page.component';
+import { MapComponent } from './component/map/map.component';
 
 export const routes: Routes = [
     {path:'',component:HomePageComponent},
+    { path:'map', component: MapComponent }
 ];
