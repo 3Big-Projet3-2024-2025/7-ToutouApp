d09213b96b4faf29ec3fd79e7d56209af530a9ed
la228388 <la228388@student.helha.be>
2024-12-20 14:57:17 +0100

    Updated authentication flow and logout

--- ToutouApp-fronted/src/app/app.routes.ts
+++ ToutouApp-fronted/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './component/home-page/home-page.component';
 import { MapComponent } from './component/map/map.component';
 import { PersonalProfileComponent } from './component/personal-profile/personal-profile.component';
+import { AuthGuard } from '../app/guard/auth.guard';
 
 export const routes: Routes = [
-    {path:'',component:HomePageComponent},
-    { path:'map', component: MapComponent },
-    { path:'personal-profile', component: PersonalProfileComponent }
-];
+    { path: '', component: HomePageComponent },
+    { path: 'map', component: MapComponent, canActivate: [AuthGuard] },
+    { path: 'personal-profile', component: PersonalProfileComponent, canActivate: [AuthGuard] },
+  ];
