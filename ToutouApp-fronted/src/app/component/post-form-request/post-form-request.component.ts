6258c11c461acff0f3d22d68f610930ff9e9ac37
la227758 <la227758@student.helha.be>
2024-12-20 20:33:05 +0100

    creation of the request post form compoment and i have added some data in the db

---
+++ ToutouApp-fronted/src/app/component/post-form-request/post-form-request.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-form-request',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-form-request.component.html',
+  styleUrl: './post-form-request.component.css'
+})
+export class PostFormRequestComponent {
+
+}
