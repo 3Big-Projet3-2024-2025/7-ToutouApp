7a74723ac8d62cd0643d7a8d0fd1187d51b41062
la227758 <la227758@student.helha.be>
2025-01-04 18:38:48 +0100

    translation in comment-helper, edit-request

--- ToutouApp/src/main/java/be/projet3/toutouapp/models/DogCategory.java
+++ ToutouApp/src/main/java/be/projet3/toutouapp/models/DogCategory.java
@@ -1,10 +1,14 @@
 package be.projet3.toutouapp.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DogCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- ToutouApp/src/main/java/be/projet3/toutouapp/models/Role.java
+++ ToutouApp/src/main/java/be/projet3/toutouapp/models/Role.java
@@ -2,10 +2,14 @@ package be.projet3.toutouapp.models;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
