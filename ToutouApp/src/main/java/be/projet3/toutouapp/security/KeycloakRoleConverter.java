da5a553343106ed1055aa6f46012b3bb3a9fb298
la227758 <la227758@student.helha.be>
2025-01-05 00:34:49 +0100

    addition of javadoc

--- ToutouApp/src/main/java/be/projet3/toutouapp/security/KeycloakRoleConverter.java
+++ ToutouApp/src/main/java/be/projet3/toutouapp/security/KeycloakRoleConverter.java
@@ -11,8 +11,24 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+/**
+ * Converter for extracting roles from a JWT token and converting them into a collection of {@link GrantedAuthority}.
+ * This class is used to convert Keycloak roles into Spring Security authorities.
+ * Implements the {@link Converter} interface from Spring Security to perform the conversion.
+ * @author Damien DeLeeuw
+ */
 public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
 
+
+    /**
+     * Converts a JWT token into a collection of {@link GrantedAuthority} by extracting the roles from the token.
+     * The roles are expected to be contained within the "realm_access" claim of the JWT.
+     * Each role is prefixed with "ROLE_" and mapped to a {@link SimpleGrantedAuthority}.
+     *
+     * @param jwt the JWT token that contains the roles in the "realm_access" claim
+     * @return a collection of {@link GrantedAuthority} representing the roles
+     * @author Damien DeLeeuw
+     */
     @Override
     public Collection<GrantedAuthority> convert(Jwt jwt) {
         Map<String, Object> realmAccess = (Map<String, Object>) jwt.getClaims().get("realm_access");
--- ToutouApp/src/main/java/be/projet3/toutouapp/services/IDogCategoryService.java
+++ ToutouApp/src/main/java/be/projet3/toutouapp/services/IDogCategoryService.java
@@ -3,6 +3,19 @@ package be.projet3.toutouapp.services;
 import be.projet3.toutouapp.models.DogCategory;
 import java.util.List;
 
+
+/**
+ * Interface for managing DogCategory entities.
+ * Defines the contract for business logic operations related to Dog Categories.
+ * @author Jaï Dusépulchre
+ */
 public interface IDogCategoryService {
+
+    /**
+     * Retrieves all DogCategory entities from the database.
+     *
+     * @return a list of all {@link DogCategory} entities
+     * @author Jaï Dusépulchre
+     */
     List<DogCategory> getAllCategories();
 }
